c01c3e78baaaa8f8bd52e732f4cdcae5e1050793
CodeandSki <a.k.hove@gmail.com>
2016-11-14 18:16:39 -0700

    Initialized React 14er's project

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; //import main component
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers/index';
+let store = createStore(reducer);
+
+import './index.css'; //add some css
+
+ReactDOM.render(
+  (
+      <Provider store={store}>
+          <App />
+      </Provider>
+  ),
+  document.getElementById('root')
